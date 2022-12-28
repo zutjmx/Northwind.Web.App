17330df68f29777180aa37b5acd4b5d7ea5a2cf7
Jesus Zuñiga <jesuszt@hotmail.com>
2022-12-28 16:41:06 -0600

    Se agregan controladores para Categorías y Regiones

---
+++ Northwind.Web.Api/Controllers/RegionsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Data;
+using Northwind.Models;
+
+namespace Northwind.Web.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NorthwindContext _context;
+
+        public RegionsController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Regions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Region>>> GetRegions()
+        {
+            return await _context.Regions.ToListAsync();
+        }
+
+        // GET: api/Regions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Region>> GetRegion(int id)
+        {
+            var region = await _context.Regions.FindAsync(id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            return region;
+        }
+
+        // PUT: api/Regions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRegion(int id, Region region)
+        {
+            if (id != region.RegionId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(region).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RegionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Regions
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Region>> PostRegion(Region region)
+        {
+            _context.Regions.Add(region);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (RegionExists(region.RegionId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetRegion", new { id = region.RegionId }, region);
+        }
+
+        // DELETE: api/Regions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRegion(int id)
+        {
+            var region = await _context.Regions.FindAsync(id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            _context.Regions.Remove(region);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RegionExists(int id)
+        {
+            return _context.Regions.Any(e => e.RegionId == id);
+        }
+    }
+}
